2bd6612b6a067ab514c21c71eb90eaedc52f7a98
sweety-wo <sweety.webashlar@gmail.com>
2016-08-29 17:31:32 +0530

    create client folder

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var bodyParser = require('body-parser');
 var app = express();
 
+app.use(express.static(path.join(__dirname, '../client')));
 app.use(bodyParser.json());
 
 // Set routes
